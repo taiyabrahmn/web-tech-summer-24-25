f2c9affd0d805102aee28f68bc774a5f58c6bee1
Taiyab Rahman <taiyabrahman77@gmail.com>
2025-07-20 10:54:14 +0600

    Add files via upload

---
+++ From.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>lab1</title>
+</head>
+<body>
+    <center>
+
+     <h1 style="color:blue ">AIUB</h1>
+     <h2 style="color:blue">Registration Form</h2>
+
+     </center>
+    <p><b>Complete The Registration</b></p>
+
+    Full Name:
+    <input type="text" name="" placeholder="Enter Full Name" id="">
+    <br>
+    <br>
+    Full Name:
+    <input type="email" name="" placeholder="Enter Email" id="">
+    <br>
+    <br>
+    Password:
+    <input type="password" name="" placeholder="Enter Password" id="">
+    <br>
+    <br>
+
+    Gender:
+    <td><input type="radio" name="des"> Male </td>
+    <td><input type="radio" name="des"> Female </td>
+    <td><input type="radio" name="des"> Other </td>
+    <br>
+    <br>
+    language Known:
+    <td><input type="radio" name="des">English</td>
+    <td><input type="radio" name="des">Bangla</td>
+    <td><input type="radio" name="des">Hindi</td>
+    <br>
+    <br>
+   Country:
+    <td><select>
+        <option value="Bangladesh">Bangladesh</option>
+        <option value="India">India</option>
+        <option value="other">other</option>
+
+
+
+    Date of Birth:
+
+
+
+
+
+    Upload Photo:
+
+
+
+
+    Comment:
+    </select>
+    <br>
+    <br>
+    <input style="background-color: rgba(31, 25, 17, 0.5);" type="submit" value="Submit">
+    
+
+
+</body>
+</html>
